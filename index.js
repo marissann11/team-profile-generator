0bbdb412754d37b418fe7385c5c1e20ae516cfd1
marissann11 <wilhite.marissa@gmail.com>
2022-01-27 00:36:46 -0500

    changed to async await syntax and added ternary operators

--- index.js
+++ index.js
@@ -1,122 +1,107 @@
-const fs = require('fs');
-const inquirer = require('inquirer');
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-const promptManager = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is your team manager's full name?"
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "What is your team manager's ID?"
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is your team manager's email?"
-        },
-        {
-            type: 'input',
-            name: 'officeNumber',
-            message: "What is your team manager's office number?"
-        },
-        {
-            type: 'list',
-            name: 'xRoads',
-            message: "What would you like to do next?",
-            choices: ["Add Engineer", "Add Intern", "I'm done building my team"]
-        }
-    ]).then (response => {
-        if (response.xRoads === "Add Engineer") {
-            promptEngineer();
-        } else if (response.xRoads === "Add Intern") {
-            promptIntern();
-        } else {
-            generatePage();
-        }
-    })
-}
-const promptEngineer = () => {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is your Engineer's full name?"
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "What is your Engineer's ID?"
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is your Engineer's email?"
-        },
-        {
-            type: 'input',
-            name: 'github',
-            message: "What is your Engineer's GitHub username?"
-        },
-        {
-            type: 'list',
-            name: 'xRoads',
-            message: "What would you like to do next?",
-            choices: ["Add Engineer", "Add Intern", "I'm done building my team"]
-        }
-    ]).then (response => {
-        if (response.xRoads === "Add Engineer") {
-            promptEngineer();
-        } else if (response.xRoads === "Add Intern") {
-            promptIntern();
-        } else {
-            generatePage();
-        }
-    })
-}
-function promptIntern() {
-    return inquirer.prompt([
-        {
-            type: 'input',
-            name: 'name',
-            message: "What is your Intern's full name?"
-        },
-        {
-            type: 'input',
-            name: 'id',
-            message: "What is your Intern's ID?"
-        },
-        {
-            type: 'input',
-            name: 'email',
-            message: "What is your Intern's email?"
-        },
-        {
-            type: 'input',
-            name: 'school',
-            message: "Where is your intern in school?"
-        },
-        {
-            type: 'list',
-            name: 'xRoads',
-            message: "What would you like to do next?",
-            choices: ["Add Engineer", "Add Intern", "I'm done building my team"]
-        }
-    ]).then (response => {
-        if (response.xRoads === "Add Engineer") {
-            promptEngineer();
-        } else if (response.xRoads === "Add Intern") {
-            promptIntern();
-        } else {
-            generatePage();
-        }
-    })
-}
+const promptManager = async () => {
+  let res = await inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your team manager's full name?",
+    },
+    {
+      type: "input",
+      name: "id",
+      message: "What is your team manager's ID?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your team manager's email?",
+    },
+    {
+      type: "input",
+      name: "officeNumber",
+      message: "What is your team manager's office number?",
+    },
+    {
+      type: "list",
+      name: "xRoads",
+      message: "What would you like to do next?",
+      choices: ["Add Engineer", "Add Intern", "I'm done building my team"],
+    },
+  ]);
+  return res.xRoads === "Add Engineer" ? promptEngineer()
+    : res.xRoads === "Add Intern" ? promptIntern()
+    : generatePage();
+};
+const promptEngineer = async () => {
+  let res = await inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your Engineer's full name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your Engineer's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your Engineer's email?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is your Engineer's GitHub username?",
+      },
+      {
+        type: "list",
+        name: "xRoads",
+        message: "What would you like to do next?",
+        choices: ["Add Engineer", "Add Intern", "I'm done building my team"],
+      },
+    ])
+    return res.xRoads === "Add Engineer" ? promptEngineer()
+    : res.xRoads === "Add Intern" ? promptIntern()
+    : generatePage();
+};
+const promptIntern = async () => {
+  let res = await inquirer.prompt([
+      {
+        type: "input",
+        name: "name",
+        message: "What is your Intern's full name?",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is your Intern's ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your Intern's email?",
+      },
+      {
+        type: "input",
+        name: "school",
+        message: "Where is your intern in school?",
+      },
+      {
+        type: "list",
+        name: "xRoads",
+        message: "What would you like to do next?",
+        choices: ["Add Engineer", "Add Intern", "I'm done building my team"],
+      },
+    ])
+    return res.xRoads === "Add Engineer" ? promptEngineer()
+    : res.xRoads === "Add Intern" ? promptIntern()
+    : generatePage();
+};
 const generatePage = () => {
-    console.log("generate page")
-}
+  console.log("generate page");
+};
 
 promptManager();
